2327995138dd57257f455b5fe5e712626aee38c4
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-09-18 19:13:46 +0800

    EXTRA: mi439: Add MiuiCamera

--- device.mk
+++ device.mk
@@ -57,3 +57,11 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/mi439/mi439-vendor.mk)
+
+# EXTRA: MiuiCamera
+ifneq ($(wildcard vendor/miuicamera),)
+$(call inherit-product, vendor/miuicamera/config.mk)
+PRODUCT_COPY_FILES += \
+    vendor/miuicamera/common/proprietary/etc/device_features/pine.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/pine.xml \
+    vendor/miuicamera/common/proprietary/etc/device_features/olive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/olive.xml
+endif
